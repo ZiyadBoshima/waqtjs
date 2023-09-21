344743ba8dde57f3b5dd2a92dea63e608add5b6f
ZiyadBoshima <137479354+ZiyadBoshima@users.noreply.github.com>
2023-09-21 21:25:28 +0400

    Fixed timezone in utils/time

--- src/utils/time/index.js
+++ src/utils/time/index.js
@@ -1,4 +1,4 @@
-export function getTime({timezone}) {
+export function getTime(timezone) {
     return Intl.DateTimeFormat('en-US', 
     {timeZone: timezone, timeStyle: 'medium', hour12: false,}).format(new Date())
 }
